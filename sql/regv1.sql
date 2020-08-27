76b357ac78b576fcd101efb81a8346c79f968efc
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-08-28 04:27:54 +0700

    Initial database schema

---
+++ sql/regv1.sql
@@ -0,0 +1,31 @@
+create database if not exists regv1;
+
+create table if not exists course
+(
+	id int auto_increment
+		primary key,
+	year tinytext null,
+	semester text null,
+	courseno tinytext null,
+	seclec text null,
+	seclab text null,
+	crelec int null,
+	crelab int null,
+	day_1 text null,
+	day_2 text null,
+	day_3 text null,
+	btime_1 text null,
+	ftime_1 text null,
+	btime_2 text null,
+	ftime_2 text null,
+	btime_3 text null,
+	ftime_3 text null,
+	room text null,
+	instructor_id_1 int null,
+	instructor_name_1 text null,
+	instructor_id_2 int null,
+	instructor_name_2 text null,
+	instructor_id_3 int null,
+	instructor_name_3 text null,
+	max int null
+);
---
+++ sql/regv2.sql
@@ -0,0 +1,43 @@
+create database if not exists regv2;
+
+create table if not exists course
+(
+	id int auto_increment
+		primary key,
+	year text null,
+	semester text null,
+	courseno text null,
+	seclec text null,
+	seclab text null,
+	crelec text null,
+	crelab text null
+);
+
+create table if not exists instructor
+(
+	id int auto_increment
+		primary key,
+	name text null
+);
+
+create table if not exists course_instructor
+(
+	course_id int null,
+	instructor_id int null,
+	constraint course_instructor_course_id_fk
+		foreign key (course_id) references course (id),
+	constraint course_instructor_instructor_id_fk
+		foreign key (instructor_id) references instructor (id)
+);
+
+create table if not exists time
+(
+	id int auto_increment
+		primary key,
+	course_id int null,
+	day text null,
+	btime text null,
+	ftime text null,
+	constraint time_course_id_fk
+		foreign key (course_id) references course (id)
+);
